b90cc16a39b5e916eb937a994dccbe1184b58abd
lisitsky <lisicky@inbox.ru>
2021-05-27 03:01:14 +0300

    update for new ndk support

--- lapack/jni/Application.mk
+++ lapack/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_STL := gnustl_static
+APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions -mfloat-abi=softfp -mfpu=neon -std=gnu++0x -Wno-deprecated \
 -ftree-vectorize -ffast-math -fsingle-precision-constant
 APP_ABI := armeabi armeabi-v7a 
